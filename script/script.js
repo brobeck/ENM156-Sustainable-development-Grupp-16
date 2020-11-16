33ac4cd15c400c60bd83ee0fd798111ac1461a5c
brobeck <axel.brobeck@gmail.com>
2020-11-16 14:53:57 +0100

    script init

---
+++ script/script.js
@@ -0,0 +1,9 @@
+window.onload = initScripts;
+
+function initScripts(){
+    document.getElementById("knapp").addEventListener("click", myScript);
+}
+
+function myScript(){
+    console.log("myScriotsafd");
+}
