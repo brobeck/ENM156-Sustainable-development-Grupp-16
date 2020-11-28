ae7add20d766ce587491c9cc6e04d4009d760570
Nermin Skenderovic <nermin.skenderovic.1999@gmail.com>
2020-11-28 22:27:25 +0100

    Make unused tickets scrollable

--- script/unusedTickets.js
+++ script/unusedTickets.js
@@ -3,42 +3,44 @@ const ticketType = 'Ungdom'
 const zone = 'A'
 
 const unusedTickets = {
-  "youth": {
-      "a": 0,
-      "ab": 0,
-      "abc": 0,
-      "b": 0,
-      "bc": 0,
-      "c": 0
+  youth: {
+    a: 1,
+    ab: 13,
+    abc: 37,
+    b: 0,
+    bc: 0,
+    c: 0,
   },
-  "adult": {
-      "a": 0,
-      "ab": 0,
-      "abc": 0,
-      "b": 0,
-      "bc": 0,
-      "c": 0
-  }
-};
+  adult: {
+    a: 0,
+    ab: 0,
+    abc: 0,
+    b: 0,
+    bc: 0,
+    c: 0,
+  },
+}
 
-const ticket = `
-  <div class="rectangle lightblue unused-ticket">
+const ticket = (quantity, type, zone) => `
+  <div class="unused-ticket">
     <div class="unused-ticket-type">
       <div class="numberOfTicketsBox">
-        <div class="numberOfTicketsBoxText">${nrTickets}</div>
+        <div class="numberOfTicketsBoxText">${quantity}</div>
       </div>
-      <span class="numberOfTicketsType">${ticketType}</span>
+      <span class="numberOfTicketsType">${type}</span>
     </div>
     <div class="unused-zone rect_center column">
-      <span class="">Zon</span>
+      <span>Zon</span>
       <span>${zone}</span>
     </div>
-      <div class=" activate-btn">Aktivera</div>
-  </div>
-`
+      <div class="activate-btn">Aktivera</div>
+  </div>`
+
+const tickets = Object.entries(unusedTickets.youth).map(([zone, quantity]) => {
+  return ticket(quantity, 'Ungdom', zone)
+})
+console.log(tickets)
+
 const ticketContainer = document.querySelector('#ticket-container')
-ticketContainer.innerHTML = ticket
 
-console.log(Object.values(unusedTickets))
-
-// rectangle thin white align_center
+tickets.forEach((ticket) => (ticketContainer.innerHTML += ticket))
